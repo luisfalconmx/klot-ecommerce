d212679feb53bce6ee635289d263f7d8baf6a267
luisfalconmx <luisfalconmx@gmail.com>
2024-04-23 15:46:19 -0600

    feat: navbar component created

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,58 @@
 import Image from "next/image";
-import Logo from "@/assets/images/logo.svg";
+import Link from "next/link";
+import IconHome from "@/assets/icons/icon-home.svg";
+import IconNotification from "@/assets/icons/icon-notification.svg";
+import IconTicket from "@/assets/icons/icon-ticket.svg";
+import IconUser from "@/assets/icons/icon-user.svg";
+import styles from "./Navbar.module.css";
 
 export const Navbar = () => (
-  <header>
-    <Image src={Logo} alt="Monito Logo" width={92} height={32} />
-  </header>
+  <nav className={styles["Navbar"]}>
+    <ul className={styles["Navbar__list"]}>
+      <li className={styles["Navbar__item"]}>
+        <Link href="/home">
+          <Image
+            src={IconHome}
+            alt=""
+            width={48}
+            height={48}
+            className={styles["Navbar__icon"]}
+          />
+        </Link>
+      </li>
+      <li className={styles["Navbar__item"]}>
+        <Link href="/notifications">
+          <Image
+            src={IconNotification}
+            alt=""
+            width={48}
+            height={48}
+            className={styles["Navbar__icon"]}
+          />
+        </Link>
+      </li>
+      <li className={styles["Navbar__item"]}>
+        <Link href="/cart">
+          <Image
+            src={IconTicket}
+            alt=""
+            width={48}
+            height={48}
+            className={styles["Navbar__icon"]}
+          />
+        </Link>
+      </li>
+      <li className={styles["Navbar__item"]}>
+        <Link href="/account">
+          <Image
+            src={IconUser}
+            alt=""
+            width={48}
+            height={48}
+            className={styles["Navbar__icon"]}
+          />
+        </Link>
+      </li>
+    </ul>
+  </nav>
 );
