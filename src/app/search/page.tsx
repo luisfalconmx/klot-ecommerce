cfd29a1eec45e02394f1dc76c9311af60dfe3e2d
luisfalconmx <luisfalconmx@gmail.com>
2024-04-25 10:43:54 -0600

    feat: filters added in search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import IconArrowLeft from "@/assets/icons/icon-arrow-left.svg";
+import IconFilter from "@/assets/icons/icon-filter.svg";
 import { ProductCard } from "@/components";
 import { Search } from "@/components";
 
@@ -13,7 +14,7 @@ interface SearchPageProps {
 export default function SearchPage({ searchParams }: SearchPageProps) {
   return (
     <main className="mt-12 mb-24">
-      <section className="mx-6 flex items-center space-x-4 mb-8">
+      <section className="mx-6 flex items-center space-x-4 mb-6">
         <Link
           href="/"
           className="p-3 bg-pearl rounded-full cursor-pointer block w-fit"
@@ -22,6 +23,49 @@ export default function SearchPage({ searchParams }: SearchPageProps) {
         </Link>
 
         <Search className="w-full" defaultValue={searchParams.term} />
+      </section>
+
+      <section className="flex space-x-2 mb-8 mx-6 flex-nowrap max-w-full overflow-auto">
+        <button className="pl-2 pr-3 py-1 bg-primary text-white cursor-pointer rounded-full flex items-center">
+          <Image
+            src={IconFilter}
+            alt=""
+            width={16}
+            height={16}
+            className="block invert mr-1"
+          />
+          Price
+        </button>
+        <button className="pl-2 pr-3 py-1 bg-pearl text-black cursor-pointer rounded-full flex items-center space-x-2">
+          <Image
+            src={IconFilter}
+            alt=""
+            width={16}
+            height={16}
+            className="block mr-1"
+          />{" "}
+          Sort by
+        </button>
+        <button className="pl-2 pr-3 py-1 cursor-pointer rounded-full bg-primary text-white flex items-center space-x-2">
+          <Image
+            src={IconFilter}
+            alt=""
+            width={16}
+            height={16}
+            className="block invert mr-1"
+          />{" "}
+          Genre
+        </button>
+        <button className="pl-2 pr-3 py-1 cursor-pointer rounded-full bg-primary text-white flex items-center space-x-2">
+          <Image
+            src={IconFilter}
+            alt=""
+            width={16}
+            height={16}
+            className="block invert mr-1"
+          />{" "}
+          Color
+        </button>
       </section>
 
       <section className="mx-6">
